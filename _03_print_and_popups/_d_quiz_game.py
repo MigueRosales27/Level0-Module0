7b0c89ba20458f755751e5f5489e485a8dd43fd9
Miguel Rosales <miguelrosales2027@gmail.com>
2022-02-09 18:27:56 -0800

    finshed logical turtle, finished shape

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,20 +1,37 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
+
+    if __name__ == '__main__':
+
+
+ # Make a new window variable, window = Tk()
+
+    window = Tk()
+
     # Hide the window using the window's .withdraw() method
-    
+
+    window.withdraw()
+
+
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
+
+    user_score=simpledialog.askstring(None, '0' )
 
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    
+
+    question_code = simpledialog.askstring(None, prompt= "How many bones are in our body?")
+
     #      // 3.  Use an if statement to check if their answer is correct
 
-    #      // 4.  if the user's answer was correct, add one to their score 
+if question_code == "206":
+
+    messagebox.showinfo(message= "correct!")
+else:
+    messagebox.showinfo(message= "Incorrect!")
+    #      // 4.  if the user's answer was correct, add one to their score
  
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
@@ -23,3 +40,4 @@ from tkinter import messagebox, simpledialog, Tk
     # remember to convert your variable to a string using the str() function 
     
     # Run the window's .mainloop() method
+    window.mainloop()
